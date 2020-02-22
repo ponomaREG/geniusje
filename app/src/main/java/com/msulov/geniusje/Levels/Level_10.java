b9e1a3ff384f97d45d8c5e71894aa17cd0111af6
disfunction <ponomarcomru@gmail.com>
2020-02-22 17:47:14 +0300

    base

--- app/src/main/java/com/msulov/geniusje/Levels/Level_10.java
+++ app/src/main/java/com/msulov/geniusje/Levels/Level_10.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -42,7 +43,9 @@ public class Level_10 extends AppCompatActivity {
     private boolean hasChoosed = false;
     private boolean hasPair = false;
     private int count, correctAnswer, correct_color;
-    private int[] array_of_numbers;
+    private int[] cells;
+    private TextView[] cells_views;
+    private Handler handler_for_timer;
 
 
 
@@ -62,7 +65,7 @@ public class Level_10 extends AppCompatActivity {
                 } else if (v.getId() == R.id.startDialogButton) {
                     dialog.dismiss();
                     new Thread(t, "Time").start();
-                    showCells();
+                    startTask();
                 }
             }
         };
@@ -167,21 +170,16 @@ public class Level_10 extends AppCompatActivity {
     // Обработчик нажатия системной кнопки "Назад" - (Конец)
 
 
-    @Deprecated
-    private void CheckCorrect(boolean hasCorrect, TextView point) {
-        if (hasCorrect) {
-            point.setBackgroundResource(R.drawable.points_green_style);
-        } else {
-            point.setBackgroundResource(R.drawable.points_red_style);
-        }
-
-    }
 
 //
 //    private void makeTask() {
 //        array_of_numbers = Cells.getRandomArrayOfNumbers(16);
 //
 //    }
+
+
+
+    //BEGIN AND SETUP
 
     private void showBeginningDialog() {
         t = new Time();
@@ -207,130 +205,211 @@ public class Level_10 extends AppCompatActivity {
         icon.setImageDrawable(getResources().getDrawable(R.drawable.level2_icon));
     }
 
-    private void showCells(){
-        int[] cells = Memory_game.getShakedArray(Memory_game.getArrayOfNumbersForGame(16));
-        t = new Time();
-        new Thread(t,"time");
 
-        for (int i = 1;i<17;i++){
-            cell = findViewById(getResources().getIdentifier("cell_"+i,"id",getPackageName()));
-            cell.setTextSize(48);
-            cell.setTag(R.string.tagClosed,0);
-            cell.setTag(R.string.tagCellNumber,cells[i-1]);
-            Log.d("array",String.valueOf(i));
-            cell.setText(String.valueOf(cells[i-1]));
 
-        }
-        Timer timer = new Timer();
-        timer.schedule(new MyTimerTask(),3000);
-
+    //START TASK
+    private void startTask(){
+        initCells();
+        showCells();
+        handler_for_timer = new Handler();
+        handler_for_timer.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                closeCells();
+                installOclToCellViews();
+            }
+        },3000);
 
     }
 
+    //INIT
+    private void initCells(){
+        cells = Memory_game.getArrayOfNumbersForGame(16);
+        cells = Memory_game.getShakedArray(cells);
+        cells_views = new TextView[16];
+        for (int i = 1;i<17;i++){
+            cells_views[i-1] = findViewById(getResources().getIdentifier("cell_"+i,"id",getPackageName()));
+            cells_views[i-1].setTextSize(48);
+        }
+    }
 
+
+    private void showCells(){
+        for (int i = 0;i<16;i++){
+            cells_views[i].setText(String.valueOf(cells[i]));
+            cells_views[i].setTag(R.string.tagClosed,1);
+            cells_views[i].setTag(R.string.tagCellNumber,cells[i]);
+        }
+    }
 
     private void closeCells(){
-        for (int i = 1;i<17;i++){
-            cell = findViewById(getResources().getIdentifier("cell_"+i,"id",getPackageName()));
-            cell.setText("");
-            setOclOnCells(cell);
+        for (TextView cell_view:cells_views){
+            cell_view.setText("");
         }
     }
 
-    private View.OnClickListener getOnClickListener(){
+
+
+    private void installOclToCellViews(){
         View.OnClickListener ocl = new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                TextView textView = (TextView) view;
-                log("1 ds",view.isEnabled());
-                log("2 ds",view.isClickable());
-                log("3 ds",view.isFocusableInTouchMode());
-                log("4 ds",view.isFocusable());
-                if (!hasChoosed) {
-
-                    if (Integer.parseInt(view.getTag(R.string.tagClosed).toString()) == 0){
-                        chossedCell = textView;
-                        textView.setText(chossedCell.getTag(R.string.tagCellNumber).toString());
-                        hasChoosed = true;
-                        Log.d("CHOOSED","1");
-                        chossedCell.setClickable(false);
-                        log("1",chossedCell.isEnabled());
-                        log("2",chossedCell.isClickable());
-                        log("3",chossedCell.isFocusableInTouchMode());
-                        log("4",chossedCell.isFocusable());
-
+            public void onClick(View v) {
+                if(v.getId()==R.id.cell_4){
+                    for (TextView cell_view:cells_views){
+                        if(!cell_view.getTag(R.string.tagCellNumber).toString().equals("8")) {
+                            cell_view.setClickable(false);
+                        }
                     }
                 }else{
-                      second_choosed_cell = textView;
-//                    frozeViews();
-
-                    if (second_choosed_cell.getTag(R.string.tagCellNumber).toString().equals(chossedCell.getTag(R.string.tagCellNumber).toString())){
-                        second_choosed_cell.setTag(R.string.tagClosed,1);
-                        chossedCell.setTag(R.string.tagClosed,1);
-                        count++;
-                        second_choosed_cell.setText(second_choosed_cell.getTag(R.string.tagCellNumber).toString());
-                        chossedCell.setText(second_choosed_cell.getTag(R.string.tagCellNumber).toString());
-                        Log.d("SUCCESS","1");
-                        Log.d("CHOOSSED",String.valueOf(chossedCell.isEnabled()));
-                        Log.d("TEXTVIEW",String.valueOf(second_choosed_cell.isEnabled()));
-                        second_choosed_cell.setClickable(false);
-                        if (count==5){
-                            startResultsDialog();
-                        }
-                        hasChoosed = false;
-
-
-                    }else{
-                        second_choosed_cell.setText(second_choosed_cell.getTag(R.string.tagCellNumber).toString());
-                        chossedCell.setText(chossedCell.getTag(R.string.tagCellNumber).toString());
-//                        second_choosed_cell = textView;
-                        chossedCell.setClickable(true);
-                        hasChoosed = false;
-                        Log.d("CHOOSSED",String.valueOf(chossedCell.isEnabled()));
-                        Log.d("TEXTVIEW",String.valueOf(textView.isEnabled()));
-                        Timer timer = new Timer();
-                        timer.schedule(new AnotherTimerTask(),1500);
-
+                    for(TextView cell_view:cells_views){
+                        cell_view.setClickable(true);
                     }
-
-                    unfrozeViews();
-
                 }
+                log("VIEW",v.getTag(R.string.tagCellNumber).toString());
             }
         };
-        return ocl;
-    }
 
-    private void setOclOnCells(TextView cell){
-        View.OnClickListener ocl = getOnClickListener();
-        cell.setOnClickListener(ocl);
-    }
-
-    private void closeCurrentCells(){
-        chossedCell.setText("");
-        second_choosed_cell.setText("");
-    }
-
-
-    private void frozeViews(){
-        for (int i = 1; i < 17; i++){
-            TextView view = findViewById(getResources().getIdentifier("cell_"+i,"id",getPackageName()));
-            if (Integer.parseInt(view.getTag(R.string.tagClosed).toString()) == 0){
-                view.setClickable(false);
-                Log.d("VIEW ENABLED",String.valueOf(view.isClickable()));
-            }
+        for (TextView cell_view :cells_views){
+            cell_view.setOnClickListener(ocl);
         }
     }
 
-    private void unfrozeViews(){
-        for (int i = 1; i < 17; i++){
-            TextView view = findViewById(getResources().getIdentifier("cell_"+i,"id",getPackageName()));
-            if (Integer.parseInt(view.getTag(R.string.tagClosed).toString())==0){
-                view.setClickable(true);
-                Log.d("VIEW ENABLED 2",String.valueOf(view.isEnabled()));
-            }
-        }
-    }
+
+
+
+
+
+
+
+
+
+
+//
+//
+//    private void showCells(){
+//        int[] cells = Memory_game.getShakedArray(Memory_game.getArrayOfNumbersForGame(16));
+//        t = new Time();
+//        new Thread(t,"time");
+//
+//        for (int i = 1;i<17;i++){
+//            cell = findViewById(getResources().getIdentifier("cell_"+i,"id",getPackageName()));
+//            cell.setTextSize(48);
+//            cell.setTag(R.string.tagClosed,0);
+//            cell.setTag(R.string.tagCellNumber,cells[i-1]);
+//            Log.d("array",String.valueOf(i));
+//            cell.setText(String.valueOf(cells[i-1]));
+//
+//        }
+//        Timer timer = new Timer();
+//        timer.schedule(new MyTimerTask(),3000);
+//
+//
+//    }
+//
+//
+//
+//    private void closeCells(){
+//        for (int i = 1;i<17;i++){
+//            cell = findViewById(getResources().getIdentifier("cell_"+i,"id",getPackageName()));
+//            cell.setText("");
+//            setOclOnCells(cell);
+//        }
+//    }
+//
+//    private View.OnClickListener getOnClickListener(){
+//        View.OnClickListener ocl = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                TextView textView = (TextView) view;
+//                log("1 ds",view.isEnabled());
+//                log("2 ds",view.isClickable());
+//                log("3 ds",view.isFocusableInTouchMode());
+//                log("4 ds",view.isFocusable());
+//                if (!hasChoosed) {
+//
+//                    if (Integer.parseInt(view.getTag(R.string.tagClosed).toString()) == 0){
+//                        chossedCell = textView;
+//                        textView.setText(chossedCell.getTag(R.string.tagCellNumber).toString());
+//                        hasChoosed = true;
+//                        Log.d("CHOOSED","1");
+//                        chossedCell.setClickable(false);
+//                        log("1",chossedCell.isEnabled());
+//                        log("2",chossedCell.isClickable());
+//                        log("3",chossedCell.isFocusableInTouchMode());
+//                        log("4",chossedCell.isFocusable());
+//
+//                    }
+//                }else{
+//                      second_choosed_cell = textView;
+////                    frozeViews();
+//
+//                    if (second_choosed_cell.getTag(R.string.tagCellNumber).toString().equals(chossedCell.getTag(R.string.tagCellNumber).toString())){
+//                        second_choosed_cell.setTag(R.string.tagClosed,1);
+//                        chossedCell.setTag(R.string.tagClosed,1);
+//                        count++;
+//                        second_choosed_cell.setText(second_choosed_cell.getTag(R.string.tagCellNumber).toString());
+//                        chossedCell.setText(second_choosed_cell.getTag(R.string.tagCellNumber).toString());
+//                        Log.d("SUCCESS","1");
+//                        Log.d("CHOOSSED",String.valueOf(chossedCell.isEnabled()));
+//                        Log.d("TEXTVIEW",String.valueOf(second_choosed_cell.isEnabled()));
+//                        second_choosed_cell.setClickable(false);
+//                        if (count==5){
+//                            startResultsDialog();
+//                        }
+//                        hasChoosed = false;
+//
+//
+//                    }else{
+//                        second_choosed_cell.setText(second_choosed_cell.getTag(R.string.tagCellNumber).toString());
+//                        chossedCell.setText(chossedCell.getTag(R.string.tagCellNumber).toString());
+////                        second_choosed_cell = textView;
+//                        chossedCell.setClickable(true);
+//                        hasChoosed = false;
+//                        Log.d("CHOOSSED",String.valueOf(chossedCell.isEnabled()));
+//                        Log.d("TEXTVIEW",String.valueOf(textView.isEnabled()));
+//                        Timer timer = new Timer();
+//                        timer.schedule(new AnotherTimerTask(),1500);
+//
+//                    }
+//
+//                    unfrozeViews();
+//
+//                }
+//            }
+//        };
+//        return ocl;
+//    }
+//
+//    private void setOclOnCells(TextView cell){
+//        View.OnClickListener ocl = getOnClickListener();
+//        cell.setOnClickListener(ocl);
+//    }
+//
+//    private void closeCurrentCells(){
+//        chossedCell.setText("");
+//        second_choosed_cell.setText("");
+//    }
+//
+//
+//    private void frozeViews(){
+//        for (int i = 1; i < 17; i++){
+//            TextView view = findViewById(getResources().getIdentifier("cell_"+i,"id",getPackageName()));
+//            if (Integer.parseInt(view.getTag(R.string.tagClosed).toString()) == 0){
+//                view.setClickable(false);
+//                Log.d("VIEW ENABLED",String.valueOf(view.isClickable()));
+//            }
+//        }
+//    }
+//
+//    private void unfrozeViews(){
+//        for (int i = 1; i < 17; i++){
+//            TextView view = findViewById(getResources().getIdentifier("cell_"+i,"id",getPackageName()));
+//            if (Integer.parseInt(view.getTag(R.string.tagClosed).toString())==0){
+//                view.setClickable(true);
+//                Log.d("VIEW ENABLED 2",String.valueOf(view.isEnabled()));
+//            }
+//        }
+//    }
 
 
     private void log(String tag,String text){
@@ -356,7 +435,7 @@ public class Level_10 extends AppCompatActivity {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            closeCells();
+//                            closeCells();
                         }
                     });
                 }
@@ -374,7 +453,7 @@ public class Level_10 extends AppCompatActivity {
 
         @Override
         public void run() {
-            closeCurrentCells();
+//            closeCurrentCells();
         }
     }
 
