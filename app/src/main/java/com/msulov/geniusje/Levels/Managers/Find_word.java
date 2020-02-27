8108d7429b2b50108480e0af2f729c180a57cb60
disfunction <ponomarcomru@gmail.com>
2020-02-27 13:13:47 +0300

    add level_14,level_15 // TODO: level_16

---
+++ app/src/main/java/com/msulov/geniusje/Levels/Managers/Find_word.java
@@ -0,0 +1,46 @@
+package com.msulov.geniusje.Levels.Managers;
+
+public class Find_word {
+    private static String[][] words = {
+            {"наборщик","борщ","рок","нора","икра","кора","икона","краб","кино","брак","кран"},
+            {"программный","гной","поры","нора","ранг","рога","йога","морг","рано","найм","пары",},
+            {"аллигатор","рога","торг","тигр","лига","тара","литр","игра","игла","грот","трио",},
+            {"когтистый","гост","итог","коты","киты","соты","скот","стык","сток","тост","стог",},
+            {"волнистый","вино","винт","стол","стон","твой","сито","иной","ноты","лист","ионы",},
+    };
+
+    private  static int[][] koef_is_found = {
+            {0,0,0,0,0,0,0,0,0,0,},
+            {0,0,0,0,0,0,0,0,0,0,},
+            {0,0,0,0,0,0,0,0,0,0,},
+            {0,0,0,0,0,0,0,0,0,0,},
+            {0,0,0,0,0,0,0,0,0,0,},
+    };
+
+
+
+    private static String[] getWords(int index){
+        return words[index];
+    }
+
+    private static int[] getKoefIsFound(int index){
+        return koef_is_found[index];
+    }
+
+    private static boolean hasFounded(int index,String word){
+        boolean hasFounded = false;
+        for (int i = 0 ;i<words[index].length;i++){
+            if((words[index][i].equals(word))&&(koef_is_found[index][i]==1)){ hasFounded = true; return hasFounded;}
+        }
+        return hasFounded;
+    }
+
+
+    private static void setKoefhasFounded(int index,String word){
+        for (int i = 0 ;i<words[index].length;i++){
+            if(words[index][i].equals(word)) koef_is_found[index][i]=1;
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/msulov/geniusje/Levels/one_choice.java
@@ -0,0 +1,17 @@
+
+package com.msulov.geniusje.Levels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.msulov.geniusje.R;
+
+public class one_choice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.one_choice);
+    }
+}
